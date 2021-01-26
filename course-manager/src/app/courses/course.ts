2a1ccd1ef198e9a61cf47f15e7f28a77a965aa2e
alexaugusto23 <contato.alexaugusto@hotmail.com>
2021-01-26 16:36:36 -0300

    crop images

--- course-manager/src/app/courses/course.ts
+++ course-manager/src/app/courses/course.ts
@@ -1,9 +1,14 @@
 export class Course {
-    id: number;
-    name: string;
-    imageUrl: string;
-    price: number;
-    code: string;
-    duration: number;
-    rating: number;
+    id?: number;
+    name?: string;
+    imageUrl?: string;
+    price?: number;
+    code?: string;
+    duration?: number;
+    rating?: number;
+    releaseDate?: string;
+
 }
+
+
+// colocar ? optional or fazer o construtor
